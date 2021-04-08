3ec6684805050fc9637d7cf0c433f70a37659002
Shakir <shakir.azimli@guavapay.com>
2021-04-08 18:48:21 +0400

    Day 0 slolition added

---
+++ HackerRank/Java/30 Days of Code/src/com/hackerrank/Day_0.java
@@ -0,0 +1,17 @@
+package com.hackerrank;
+
+import java.util.Scanner;
+
+public class Day_0 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String message = scanner.nextLine();
+
+        System.out.println("Hello, World.");
+        System.out.println(message);
+        scanner.close();
+    }
+}
